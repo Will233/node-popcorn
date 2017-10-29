311ecd3e68ada92bff22294626ca2173b1e11318
Will233 <s2573864@163.com>
2017-10-29 19:36:07 +0800

    分词引擎改为:jieba

--- python/divider.py
+++ python/divider.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import thulac
+import jieba
+import codecs
 import sys
+
+# 获取调用参数：
 print "name: ", sys.argv[0]
 for i in range(1, len(sys.argv)):#这里参数从1开始
     print "param", i, sys.argv[i]
@@ -9,13 +12,45 @@ rawFileName = sys.argv[1]
 dataFileName = sys.argv[2]
 tmpFileName =  sys.argv[3]
 sortFileName = sys.argv[4]
-# 分词
-thu1 = thulac.thulac(seg_only=True) #默认模式
-# text = thu1.cut("我爱北京天安门", text=False) #一句话分词
-thu1.cut_f(rawFileName, dataFileName) # 文本文件分词
+# @see 读取文件内容
+def readFile(filename):
+  content = ""
+  try:
+    fo = open(filename)
+    print "读取文件名：", filename
+    for line in fo.readlines():
+      content += line.strip()
+    print "字数：", len(content)
+  except IOError as e:
+    print "文件不存在或者文件读取失败"
+    return ""
+  else:
+    fo.close()
+    return content
+# @see 写入文件内容（数组会使用writelines进行写入）codec.open实现
+# @param toFile 文件名
+#        content 内容
+def writeFile(toFile, content):
+  try:
+    fo = codecs.open(toFile, 'wb', "utf-8")
+    print "文件名：", toFile
+    if type(content) == type([]):
+      fo.writelines(content)
+    else:
+      fo.write(content)
+  except IOError:
+    print "没有找到文件或文件读取失败"
+  else:
+    print "文件写入成功"
+    fo.close()
+
+# 结巴分词
+rawContent = readFile(rawFileName)
+seg_list = jieba.cut(rawContent, cut_all=False)
+writeFile(dataFileName, " ".join(seg_list))
 
 
-# 排序
+# 词频统计和排序
 word_lst= []
 word_dict= {}
 word_items= []
@@ -26,6 +61,7 @@ def cmp_times (x, y):
     return -1
   else:
     return 0
+# 定义wordItem 类
 class wordItem:
   label = ''
   times = 0
@@ -52,4 +88,3 @@ with open(dataFileName) as wf, open(sortFileName,'w') as wf2, open(tmpFileName, 
   word_items.sort(reverse = True)
   for item in word_items:
       wf2.write(item.label+' '+str(item.times) + '\n')
-# raw_input() #added
