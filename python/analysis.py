08ce52fd181aeec47d5d7de21a7dd3fd7c6e2c32
Will233 <s2573864@163.com>
2017-10-29 23:40:14 +0800

    优化统计过程

--- python/analysis.py
+++ python/analysis.py
@@ -2,6 +2,38 @@
 import codecs
 import sys
 
+# @see 读取文件内容
+def readFile(filename):
+  content = ""
+  try:
+    fo = codecs.open(filename, 'r', "utf-8")
+    print "读取文件名：", filename
+    for line in fo.readlines():
+      content += line.strip()
+    print "字数：", len(content)
+  except IOError as e:
+    print "文件不存在或者文件读取失败"
+    return ""
+  else:
+    fo.close()
+    return content
+# @see 写入文件内容（数组会使用writelines进行写入）codec.open实现
+# @param toFile 文件名
+#        content 内容
+def writeFile(toFile, content):
+  try:
+    fo = codecs.open(toFile, 'wb', "utf-8")
+    print "文件名：", toFile
+    if type(content) == type([]):
+      fo.writelines(content)
+    else:
+      fo.write(content)
+  except IOError:
+    print "没有找到文件或文件读取失败"
+  else:
+    print "文件写入成功"
+    fo.close()
+
 print "\n###### Analysis Start ######\n"
 # 获取调用参数：
 # print "name: ", sys.argv[0] # 脚本名
@@ -9,9 +41,9 @@ sourceFile = sys.argv[1]
 targetFile = sys.argv[2]
 
 # 词频统计和排序
-word_lst= []
-word_dict= {}
-word_items= []
+wordList= []
+wordCount= {}
+wordItemArray= []
 
 # 定义wordItem 类
 class wordItem:
@@ -24,24 +56,23 @@ class wordItem:
     return self.times < other.times
 
 # 读取数据并分析
-with codecs.open(sourceFile, 'r', "utf-8") as wf, codecs.open(targetFile,'w', "utf-8") as wf2:
+wf = codecs.open(targetFile,'w', "utf-8")
+# 读取数据并分析
+sourceData = readFile(sourceFile)
+wordList = sourceData.split(' ')
+for item in wordList:
+  if item not in wordCount:
+    wordCount[item] = 1
+  else:
+    wordCount[item] += 1
 
-  for word in wf:  
-        word_lst.append(word.split(' ')) 
-        for item in word_lst:  
-             for item2 in item:  
-                if item2 not in word_dict:
-                    word_dict[item2] = 1  
-                else:  
-                    word_dict[item2] += 1  
+for key in wordCount:
+  wordItemArray.append(wordItem(key, wordCount[key]))
 
-  for key in word_dict:  
-      word_items.append(wordItem(key, word_dict[key]))
+wordItemArray.sort(reverse = True)
 
-  word_items.sort(reverse = True)
-  for item in word_items:
-      wf2.write(item.label+' '+str(item.times) + '\n')
-
+for item in wordItemArray:
+  wf.write(item.label+' '+str(item.times) + '\n')
 print "源文件", sourceFile
 print "目标文件", targetFile
 print "\n###### Analysis Finish ######"
